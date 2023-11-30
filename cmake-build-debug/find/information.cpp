7c7ebb738ae4ea876be5174e27228d8f432d476e
itpeng789 <itpeng007@outlook.com>
2023-11-30 19:29:36 +0800

    string 的应用

---
+++ cmake-build-debug/find/information.cpp
@@ -0,0 +1,18 @@
+//
+// Created by itpeng0077 on 2023/11/27.
+//
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+int main(){
+   std::string s1 = std::string("hello world");
+    std::string s2 = s1;//s2是s1的副本
+    std::cout<<"string s1 = "<<s1<<"\n";
+    std::cout<<"string s2 = "<<s2<<"\n";
+
+
+
+    system("pause");
+    return 0;
+}
