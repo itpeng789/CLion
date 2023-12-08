2b88f9119a4583b65becdfbd0278cb1cb25b0167
itpeng789 <itpeng007@outlook.com>
2023-12-08 19:04:41 +0800

    c++中整形中所占的字节大小

---
+++ cmake-build-debug/find/find4.cpp
@@ -0,0 +1,16 @@
+//
+// Created by itpeng0077 on 2023/12/6.
+//
+#include <bits/stdc++.h>
+int main(){
+    std::cout << "Size of char : " << sizeof(char)<< "\n";
+    std::cout << "Size of int : " << sizeof(int)<<"\n";
+    std::cout << "Size of short int : " << sizeof(short int)<<"\n";
+    std::cout << "Size of long int : " << sizeof(long int)<<"\n";
+    std::cout << "Size of float : " << sizeof(float)<<"\n";
+    std::cout << "Size of double : " << sizeof(double)<<"\n";
+    std::cout << "Size of wchar_t : " << sizeof(wchar_t)<<"\n";
+
+    system("pause");
+    return 0;
+}
