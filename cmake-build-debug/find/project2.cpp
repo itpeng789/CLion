a24fab9e4669524201e6423c3c90de3bc976d701
itpeng789 <itpeng007@outlook.com>
2023-12-15 10:02:47 +0800

    数组排序和查找功能

---
+++ cmake-build-debug/find/project2.cpp
@@ -0,0 +1,61 @@
+//
+// Created by itpeng0077 on 2023/12/13.
+//
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using std::cout;
+using std::cin;
+int main(){
+   int fin[] = {9,8,7,6,5,4,3,2,1};
+   int i = sizeof(fin)/sizeof(fin[0]);
+
+    int ren = 0,ret;
+    int lin;
+    int lind;
+    for(ren = 0; ren < i; ren++)
+    {
+        for(ret = ren + 1; ret < i; ret++)
+        {
+            if(fin[ren] > fin[ret])
+            {
+                lin = fin[ret];
+                fin[ret] = fin[ren];
+                fin[ren] = lin;
+            }
+        }
+    }
+/*
+* 排序循环
+*/
+  for(int w  = 0; w < i;w++){
+        cout<<"find["<<w<<"] = "<<fin[w]<<"\n";
+    }
+
+/*
+*   查找下标函数
+*/
+     int left = 0;
+     int right = i -1;
+    cout<<"Please a number : "<<"\n";
+    cin >> lind;
+    while(left <= right){
+        int mid = (right - left) / 2;
+        int find = fin[mid];
+        if(find < lind){
+            left = mid + 1;
+        }
+        else if(lind < find){
+            right = mid -1;
+        }
+        else if (lind == find ) {
+            cout << "find = " << mid << "\n";
+            break;
+        }
+    }
+
+
+
+    system("pause");
+    return 0;
+}
